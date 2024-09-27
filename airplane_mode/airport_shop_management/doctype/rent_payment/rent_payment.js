a8a7ba8bb63e29d5dd334be3a4b2c77c59fbed2d
Rajneesh2008 <rajneesh.yadav@redsoftware.in>
2024-09-27 04:16:36 +0000

    working on final assignment feature

--- airplane_mode/airport_shop_management/doctype/rent_payment/rent_payment.js
+++ airplane_mode/airport_shop_management/doctype/rent_payment/rent_payment.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2024, Rajneesh yadav and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Rent Payment", {
-// 	refresh(frm) {
+frappe.ui.form.on("Rent Payment", {
+	refresh(frm) {
+    console.log(frm.doc)
+      if(frm.doc?.shop){
+       frappe.db.get_doc('Shop', frm.doc.shop)
+        .then(doc => {
+            console.log(doc)
+            frm.doc.tenant = doc.tenant
+            frm.save()
+        })
+      }
 
-// 	},
-// });
+	},
+});
--- airplane_mode/airport_shop_management/doctype/shop/shop.js
+++ airplane_mode/airport_shop_management/doctype/shop/shop.js
@@ -1,8 +1,63 @@
 // Copyright (c) 2024, Rajneesh yadav and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Shop", {
-// 	refresh(frm) {
+frappe.ui.form.on("Shop", {
+  refresh(frm) {
+      if(!frm.doc?.is_leach){
 
-// 	},
-// });
+        if(frm.doc?.tenant){
+          frm.set_value("is_leach",1)
+          frm.save()
+        }
+
+      }
+      // Fetch all Shop records to calculate total and available shops
+      frappe.call({
+          method: "frappe.client.get_list",
+          args: {
+              doctype: "Shop",
+              fields: ["name", "status"]
+          },
+          callback: function(response) {
+              if(response.message) {
+                  let total_shops = response.message.length;
+                  let available_shops = response.message.filter(shop => shop.status === "Available").length;
+
+                  // Set the values in the form fields
+                  frm.set_value('total_shop', total_shops);
+                  frm.set_value('available_shop', available_shops);
+
+                  frm.refresh_field('total_shop');
+                  frm.refresh_field('available_shop');
+              }
+          }
+      });
+  },
+
+  // Trigger whenever the 'status' field is changed
+  status(frm) {
+      frappe.call({
+          method: "frappe.client.get_list",
+          args: {
+              doctype: "Shop",
+              fields: ["name", "status"]
+          },
+          callback: function(response) {
+              if(response.message) {
+                  let total_shops = response.message.length;
+                  let available_shops = response.message.filter(shop => shop.status === "Available").length;
+
+                  // Update the available_shop and total_shop fields
+                  frm.set_value('total_shop', total_shops);
+                  frm.set_value('available_shop', available_shops);
+
+                  frm.refresh_field('total_shop');
+                  frm.refresh_field('available_shop');
+              }
+          }
+      });
+  },
+
+ 
+});
+
